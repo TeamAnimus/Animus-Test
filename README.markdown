5d94a985ab86ff3bf36779c4b7a7d438aeaafc32
geppettodivacin <ericdilmore@gmail.com>
2014-07-22 02:14:22 -0500

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,10 @@
+Animus
+======
+This is the new repository for our Animus code. If you want to try something
+out, make a branch! If you come up with something good, send a pull request so
+that we can all take a look at it! If there are some minor changes that you want
+to make, feel free to push directly to master, but *only* if those changes are
+small and inconsequential.
+
+Don't forget to pull / rebase the current files before you begin work, every
+time you begin!
---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.Animus;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Animus";
+		cfg.width = 960;
+		cfg.height = 640;
+		new LwjglApplication(new Animus(), cfg);
+	}
+}
